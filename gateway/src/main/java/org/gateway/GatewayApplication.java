d546fe7ec2f16386974e84ce7d99b781a2d7b689
devquy <devquy@devquy-mbp.mynet>
2017-04-05 23:37:55 +0300

    initialized

---
+++ gateway/src/main/java/org/gateway/GatewayApplication.java
@@ -0,0 +1,23 @@
+package org.gateway;
+
+import org.gateway.filter.SimpleFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public SimpleFilter zuulFilter() {
+        return new SimpleFilter();
+    }
+}
