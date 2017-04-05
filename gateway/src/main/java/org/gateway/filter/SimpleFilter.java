305c27feaccc9fc58347a3950006f6bb77a7da61
devquy <devquy@devquy-mbp.mynet>
2017-04-05 23:33:26 +0300

    spring cloud zuul proxy added

---
+++ gateway/src/main/java/org/gateway/filter/SimpleFilter.java
@@ -0,0 +1,7 @@
+package org.gateway.filter;
+
+/**
+ * Created by devquy on 5.04.2017.
+ */
+public class SimpleFilter {
+}
--- service-1/src/main/java/org/microservices/example/Service1Controller.java
+++ service-1/src/main/java/org/microservices/example/Service1Controller.java
@@ -1,10 +1,12 @@
 package org.microservices.example;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping(value = "/hellos")
 public class Service1Controller {
 
     @GetMapping(value = "/hi")
