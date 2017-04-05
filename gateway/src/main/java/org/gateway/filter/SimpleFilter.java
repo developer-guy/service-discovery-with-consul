c4c3d9f7bdab1cb60519788e0d355c1b65710534
devquy <devquy@devquy-mbp.mynet>
2017-04-05 23:37:31 +0300

    initialized

--- gateway/src/main/java/org/gateway/filter/SimpleFilter.java
+++ gateway/src/main/java/org/gateway/filter/SimpleFilter.java
@@ -1,7 +1,44 @@
 package org.gateway.filter;
 
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by devquy on 5.04.2017.
  */
-public class SimpleFilter {
+
+
+public class SimpleFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(SimpleFilter.class);
+
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+
+        return null;
+    }
 }
